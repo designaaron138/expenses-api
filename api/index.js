1bc766f3f75367718ce8fbf3bf90619c161c213e
designaaron138 <84100243+designaaron138@users.noreply.github.com>
2021-05-25 09:49:20 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { createServer } = require("http");
 // 👉 Replace this with express-oauth2-bearer require 👈
+const { auth } = require("express-oauth2-bearer");
 const {
   checkUrl,
   APP_URL, // Public URL for this app
@@ -48,6 +49,9 @@ app.get("/total", (req, res) => {
 });
 
 // 👉 Replace this with auth middleware 👈
+// 👆 public routes above 👆
+app.use(auth());
+// 👇 private routes below 👇
 
 app.get("/reports", (req, res) => {
   res.send(expenses);
